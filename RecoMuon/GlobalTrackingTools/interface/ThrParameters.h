a65ccf9fa4ccc2169b4410b63f3616dcb7c57233
alessio <alessio.magitteri@cern.ch>
2015-01-27 19:07:41 +0100

    Implemented Fitting Function Using Parameters From DB

--- RecoMuon/GlobalTrackingTools/interface/ThrParameters.h
+++ RecoMuon/GlobalTrackingTools/interface/ThrParameters.h
@@ -1,6 +1,9 @@
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "CondFormats/RecoMuonObjects/interface/DYTThrObject.h"
 #include "CondFormats/DataRecord/interface/DYTThrObjectRcd.h"
+#include "CondFormats/RecoMuonObjects/interface/DYTParamObject.h"
+#include "CondFormats/RecoMuonObjects/interface/DYTParamsObject.h"
+#include "CondFormats/DataRecord/interface/DYTParamsObjectRcd.h"
 #include "DataFormats/MuonDetId/interface/DTChamberId.h"
 #include "DataFormats/MuonDetId/interface/CSCDetId.h"
 #include "DataFormats/GeometryCommonDetAlgo/interface/GlobalError.h"
@@ -21,16 +24,26 @@ class ThrParameters {
   ThrParameters(const edm::EventSetup*);
   ~ThrParameters();
 
+  //Thrs Method
   void setInitialThr(double thr0) {x0 = thr0;};
   const bool isValidThdDB() {return isValidThdDB_;};
   const std::map<DTChamberId, GlobalError>& GetDTApeMap() {return dtApeMap;};
   const std::map<CSCDetId, GlobalError>& GetCSCApeMap() {return cscApeMap;};
   const DYTThrObject* getInitialThresholds() {return dytThresholds;}
 
+  //Parameters Method
+  const bool isValidParamDB() {return isValidParamDB_;};
+  const std::map<uint32_t, std::vector<double> >& GetParametersMap() {return ParametersMap;}
+
  private:
   double x0;
   bool isValidThdDB_;
+  bool isValidParamDB_;
   const DYTThrObject* dytThresholds;
+  const DYTParamsObject* dytParams;
+  std::vector<DYTParamObject> dytParameters;
+  std::string function;
   std::map<DTChamberId, GlobalError> dtApeMap;
   std::map<CSCDetId, GlobalError> cscApeMap;
+  std::map<uint32_t,std::vector<double> > ParametersMap;
 };
